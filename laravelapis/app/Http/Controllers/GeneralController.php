3734869b7f7680f8f1e0d7e861704e4b49080ea4
Peter Saba <petersaba001@gmail.com>
2022-09-30 13:05:37 +0300

    backend: arranging chars in the right order

--- laravelapis/app/Http/Controllers/GeneralController.php
+++ laravelapis/app/Http/Controllers/GeneralController.php
@@ -16,12 +16,12 @@ class GeneralController extends Controller
 
 
         foreach ($str_arr as $value){
-            $ascii = ord($value);
-            if($ascii >= 48 && $ascii <= 57){
+            $ascii = ord($value); // getting the ascii code of the current char
+            if($ascii >= 48 && $ascii <= 57){ // numbers have ascii codes between 48 and 57
                 $numbers[] = $value;
-            }else if($ascii >= 65 && $ascii <= 90){
+            }else if($ascii >= 65 && $ascii <= 90){ // upper case characters have ascii codes between 65 and 90
                 $upper_case[] = $value; 
-            }else if($ascii >= 97 && $ascii <= 122){
+            }else if($ascii >= 97 && $ascii <= 122){ // lower case characters have ascii codes between 97 and 122
                 $lower_case[] = $value;
             }      
         }
@@ -30,5 +30,32 @@ class GeneralController extends Controller
         sort($upper_case);
         sort($numbers);
 
+        $i = 0;
+        $j = 0;
+        $result = [];
+
+        // inspired from the merge algorithm
+        while(isset($upper_case[$i]) && isset($lower_case[$j])){
+            $upper_case_ascii = ord($upper_case[$i]);
+            $lower_case_ascii = ord($lower_case[$j]);
+            // the ascii codes of lower case characters are the same as the upper case character code + 32
+            if($upper_case_ascii >= $lower_case_ascii - 32){
+                $result[] = $lower_case[$j++];
+            }else {
+                $result[] = $upper_case[$i++];
+            }
+        }
+
+        while(isset($upper_case[$i])){
+            $result[] = $upper_case[$i++];
+        }
+        
+        while(isset($lower_case[$j])){
+            $result[] = $lower_case[$j++];
+        }
+
+        $result = array_merge($result, $numbers);
+
+        return $result;
     }
 }
